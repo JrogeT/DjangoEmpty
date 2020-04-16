9486d5ae1af65099ab4f8e629b3d437f21887bb8
Jorge Rodrigo Torrez Aramayo <jorgerodrigotorrez@gmail.com>
2020-04-15 23:44:44 -0400

    Empty Project uploaded

--- djangoempty/settings.py
+++ djangoempty/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'djangoempty.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'polls/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,10 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Home</h1>")
+    return render(request,"home.html",{"title":"Home"})
 def test(request):
-    return HttpResponse("<h1>Test</h1>")
+    return render(request,"test.html",{"title":"Test"})
 def page(request):
-    return HttpResponse("<h1>Page</h1>")
+    return HttpResponse("Message")
